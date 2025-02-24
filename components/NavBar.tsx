57fe0d1ad36340b0b83f7ba0962e78684a168f3c
SaladbarAlex <alexsalazarmartinez@gmail.com>
2025-02-24 11:45:43 -0500

    fleshing out system

---
+++ components/NavBar.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import ThemeToggle from "./ThemeToggle";
+const NavbarLinks = [
+    { label: "About", href: "#about" },
+    { label: "Collaboration", href: "#collaboration" },
+    { label: "Experience", href: "#experience" },
+    { label: "Contact", href: "#contact" },
+];
+const NavBar = () => {
+    return (
+        <nav className="flex fixed items-center justify-between w-full p-5 px-24">
+            <Link href="/">
+                <Image
+                    src="/test.svg"
+                    className="text-white"
+                    width={50}
+                    style={{ fill: "#ffffff" }}
+                    height={50}
+                    alt="Logo"
+                ></Image>
+            </Link>
+            <ul className="space-x-7 flex items-center">
+                {NavbarLinks.map((link) => (
+                    <Link
+                        className="font-semibold dark:text-lightText text-zinc-500 hover:text-zinc-800"
+                        key={link.label}
+                        href={link.href}
+                    >
+                        {link.label}
+                    </Link>
+                ))}
+            </ul>
+            <ThemeToggle></ThemeToggle>
+        </nav>
+    );
+};
+
+export default NavBar;
---
+++ components/homepage/Introduction.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Button } from "../ui/button";
+import Writer from "../Writer";
+
+const Introduction = () => {
+    return (
+        <div className="h-screen w-full flex items-center justify-center ">
+            <div className="flex items-center w-fit">
+                <div className="flex items-center justify-end">
+                    <model-viewer
+                        src="/greeting_waving_110_frames_loop.glb"
+                        alt="A 3D model"
+                        autoplay
+                        camera-controls
+                        disable-zoom
+                        shadow-intensity="2"
+                        camera-orbit="-15deg 80deg 5m"
+                        style={{ width: "320px", height: "500px" }}
+                    ></model-viewer>
+                </div>
+                <div className="flex flex-col justify-center">
+                    <p className="text-2xl font-black dark:text-lightText">Hi! I'm </p>
+                    <h1 className="font-black text-8xl tracking-wide break-keep dark:text-lightText">
+                        Julius Chan
+                    </h1>
+                    <p className="h-[12px] text-2xl font-bold dark:italic dark:text-red-600">
+                        <Writer />
+                    </p>
+                    <Button className="w-fit bg-transparent mt-9 text-zinc-800  border-zinc-800  border-2 font-bold hover:text-white hover:bg-zinc-800 text-lg py-6 dark:text-lightText dark:border-lightText dark:hover:bg-lightText dark:hover:text-zinc-800">
+                        Portfolio
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Introduction;
