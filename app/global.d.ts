57301f8e786f981c800a0adfc6de717f59e2ad3e
SaladbarAlex <alexsalazarmartinez@gmail.com>
2025-02-24 12:03:03 -0500

    fixing bugs

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,7 +1,9 @@
-export declare global {
+declare global {
     namespace JSX {
         interface IntrinsicElements {
             "model-viewer": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
         }
     }
 }
+
+export {}; // Ensures this file is treated as a module.
